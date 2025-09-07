2aad691ea9096663812431b4b8ffab5999d1fb1a
Ggdriper <mihailmetlaev654@gmail.com>
2025-09-07 15:36:22 +1000

    Обновлен Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,76 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+object AppConfig {
+    val databasePassword: String by lazy {
+        println("Загружаем пароль из файла...")
+        "mySecretPassword123"
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
     }
+}
+
+fun main() {
+
+
+    var count = 10
+    count += 5
+    println(count)
+
+    println("Обращаемся к паролю впервые:")
+    println("Пароль: ${AppConfig.databasePassword}")
+    println("Пароль: ${AppConfig.databasePassword}")// загружает уже из памяти
+
+    lateinit var username: String
+    username = "Иван"
+    println("Добро пожаловать, $username!")
+
+    var name: String = "Kotlin"
+
+    var nullableName: String? = null
+
+    fun printLength(str: String?) {
+        println(str?.length ?: 0)
+    }
+
+    printLength(name)         // 6
+    printLength(nullableName) // Нет данных
+
+    fun sum(a: Int, b: Int): Int {
+        return a + b
+    }
+
+//    data class Product(var id: Int, var name: String, var price: Double)
+//    var pr1 = Product(1, "cocos" , 20.0)
+//    var pr2 = pr1.copy( name = "dundu")
+//
+//    println(pr1)
+//    println(pr2)
+
+
+    data class Product(val id: Int, val name: String, val price: Double)
+
+    val products = listOf(
+        Product(1, "Телефон", 29999.0),
+        Product(2, "Ноутбук", 89999.0),
+        Product(3, "Наушники", 4999.0),
+        Product(4, "Мышь", 1999.0)
+    )
+
+    // Преобразуем в список названий
+    val productNames = products.map { it.name }
+    println("Названия товаров: $productNames")
+    // Названия товаров: [Телефон, Ноутбук, Наушники, Мышь]
+
+    // Можно добавить дополнительную логику
+    val formattedNames = products.map { "${it.name} - ${it.price}₽" }
+    println("Форматированные названия: $formattedNames")
+
+
+
+
+
+
+
+
+
+
 }
